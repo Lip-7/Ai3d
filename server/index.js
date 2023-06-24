b96371670afd916ec43cfe7d3329e12383fe2260
Lip-7 <zhighofficial@gmail.com>
2023-06-24 12:23:38 +0200

    import and set main route in index.js

--- server/index.js
+++ server/index.js
@@ -2,11 +2,16 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 
+import dalleRoutes from './routes/dalle.routes'
+
 dotenv.config();
 
 const app = express();
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
+
+
+app.use('/api/v1/dalle', dalleRoutes)
 
 app.get('/', (reg, res) => {
     res.status(200).json({message: "Hello from DALL.E"})
