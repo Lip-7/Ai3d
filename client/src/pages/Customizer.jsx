bfdeee19b6509a9408b61fb96f2389a363e028be
Lip-7 <zhighofficial@gmail.com>
2023-06-24 14:24:34 +0200

    trying to making working the ia

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -51,7 +51,7 @@ const Customizer = () => {
         try {
             //chiamata al backend
             setGeneratingImg(true);
-            const response = await fetch('localhost:8080/api/v1/dalle', {
+            const response = await fetch('http://localhost:8080/api/v1/dalle', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -60,6 +60,8 @@ const Customizer = () => {
                     prompt
                 })
             })
+            const data = await response.json();
+            handleDecals(type, `data:image/png;base64,${data.photo}`)
         } catch (error) {
             alert(error)
         } finally {
